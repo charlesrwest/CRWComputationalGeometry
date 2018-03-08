effc9fb03ca68e12bc2f0da8a99eb235d60ca3b7
Charles West <crwest@ncsu.edu>
2018-03-08 10:19:22 -0500

    Added normals in the material related data.

--- src/library/Mesh.hpp
+++ src/library/Mesh.hpp
@@ -14,6 +14,7 @@ public:
 std::vector<Vertex> Vertices;
 std::vector<Triangle> Triangles;
 
+std::vector<Vector3d> VertexNormals;
 std::vector<std::vector<Vector2d>> UVChannelCoordinates; //[channel][vertexIndex] 
 std::vector<int64_t> MaterialSectionBounds; //Each entry holds one past the last triangle index of the given section (10 triangles starting at 0 would have 10 in position [0])
 };
